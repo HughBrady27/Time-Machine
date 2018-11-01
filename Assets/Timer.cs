75dac4a080734935bf63495b1ac078a6622867c1
HUGHBRADY <G00338260@gmit.ie>
2018-11-01 18:29:09 +0000

    Timer added

---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 300;
+    public GameObject timeUI;
+    	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timeUI.gameObject.GetComponent<Text>().text = "Time: " + (int) timeLeft;
+
+        if (timeLeft < 0.1f)
+        {
+            // Lose condition
+        }
+	}
+}
